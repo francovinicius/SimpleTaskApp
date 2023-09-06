87ed923371070d21cb5fc1316e76ebaf977c988b
francovinicius <duartevini.dev@gmail.com>
2023-09-06 14:40:52 -0300

    add navegação da página de entrada para página de login

--- app/src/main/java/com/simpletask/task/ui/SplashFragment.kt
+++ app/src/main/java/com/simpletask/task/ui/SplashFragment.kt
@@ -1,19 +1,44 @@
 package com.simpletask.task.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.simpletask.task.R
+import com.simpletask.task.databinding.FragmentSplashBinding
 
 class SplashFragment: Fragment(){
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //determinar tempo pra tela inicial ficar visivel
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 2000)
+    }
+
+
+    //verificar se o usuário está logado
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
