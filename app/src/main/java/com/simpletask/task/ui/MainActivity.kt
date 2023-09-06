ccedfedc2dd4147ff04e81cea05bc2ddd26f7469
francovinicius <duartevini.dev@gmail.com>
2023-09-06 13:27:46 -0300

    add navigation

--- app/src/main/java/com/simpletask/task/ui/MainActivity.kt
+++ app/src/main/java/com/simpletask/task/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.simpletask.task
+package com.simpletask.task.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simpletask.task.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
