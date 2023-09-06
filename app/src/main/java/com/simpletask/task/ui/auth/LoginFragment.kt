f39450dc774104059bbcd671d541b2ee05846cfc
francovinicius <duartevini.dev@gmail.com>
2023-09-06 14:55:40 -0300

    add navigation Register and Recover

--- app/src/main/java/com/simpletask/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/simpletask/task/ui/auth/LoginFragment.kt
@@ -5,18 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.simpletask.task.R
+import com.simpletask.task.databinding.FragmentLoginBinding
+import com.simpletask.task.databinding.FragmentSplashBinding
 
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
