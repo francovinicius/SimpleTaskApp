bdb3ceea837857e264085818d0dd525949b0f267
francovinicius <duartevini.dev@gmail.com>
2023-09-28 20:39:17 -0300

    add icon

---
+++ app/src/main/java/com/simpletask/task/ui/adapter/ViewPagerLayout.kt
@@ -0,0 +1,29 @@
+package com.simpletask.task.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerLayout(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+
+    private val fragmentList: MutableList<Fragment> = ArrayList()
+
+    private val tittleList: MutableList<String> = ArrayList()
+
+    fun getTitle(position: Int): String {
+        return tittleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        tittleList.add(title)
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+}
